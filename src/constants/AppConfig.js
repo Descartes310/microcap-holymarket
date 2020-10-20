d11c5291aa3de6fa7290f63e4882131873ac1b6e
PatrissolJuns <patrissolkenfack@gmail.com>
2020-10-21 00:49:39 +0100

    Updated general config

--- src/constants/AppConfig.js
+++ src/constants/AppConfig.js
@@ -2,8 +2,8 @@
  * App Config File
  */
 const AppConfig = {
-   appLogo: require('Assets/img/site-logo.png'),          // App Logo
-   brandName: 'Reactify',                                    // Brand Name
+   appLogo: require('Assets/identity/logomicrocap.png'),          // App Logo
+   brandName: 'Microcap',                                    // Brand Name
    navCollapsed: false,                                      // Sidebar collapse
    darkMode: false,                                          // Dark Mode
    boxLayout: false,                                         // Box Layout
@@ -19,6 +19,80 @@ const AppConfig = {
       name: 'English',
       icon: 'en',
    },
+   languages: [
+      {
+         languageId: 'english',
+         locale: 'en',
+         name: 'English',
+         icon: 'en',
+      },
+      {
+         languageId: 'chinese',
+         locale: 'zh',
+         name: 'Chinese',
+         icon: 'zh',
+      },
+      {
+         languageId: 'russian',
+         locale: 'ru',
+         name: 'Russian',
+         icon: 'ru',
+      },
+      {
+         languageId: 'hebrew',
+         locale: 'he',
+         name: 'Hebrew',
+         icon: 'he',
+      },
+      {
+         languageId: 'french',
+         locale: 'fr',
+         name: 'French',
+         icon: 'fr',
+      },
+      {
+         languageId: 'saudi-arabia',
+         locale: 'ar',
+         name: 'Arabic',
+         icon: 'ar',
+      },
+      {
+         languageId: 'german',
+         locale: 'de',
+         name: 'German',
+         icon: 'de',
+      },
+      {
+         languageId: 'spanish',
+         locale: 'es',
+         name: 'Spanish',
+         icon: 'es',
+      },
+      {
+         languageId: 'japanese',
+         locale: 'ja',
+         name: 'Japanese',
+         icon: 'ja',
+      },
+      {
+         languageId: 'korean',
+         locale: 'ko',
+         name: 'Korean',
+         icon: 'ko',
+      },
+      {
+         languageId: 'italian',
+         locale: 'it',
+         name: 'Italian',
+         icon: 'it',
+      },
+      {
+         languageId: 'hungarian',
+         locale: 'hu',
+         name: 'Hungarian',
+         icon: 'hu',
+      }
+   ],
    enableUserTour: process.env.NODE_ENV === 'production' ? true : false,  // Enable / Disable User Tour
    copyRightText: 'Reactify © 2019 All Rights Reserved.',      // Copy Right Text
    // light theme colors
@@ -40,7 +114,17 @@ const AppConfig = {
    // dark theme colors
    darkThemeColors: {
       darkBgColor: '#424242'
+   },
+   minPasswordLength: 3,
+   oauth: {
+      clientId: 'microcap-app',
+      clientSecret: 'geloka-secret',
+      grantType: 'password',
+   },
+   api: {
+      baseUrl: 'http://prp.microcap.fr/',
+      version: ''
    }
-}
+};
 
 export default AppConfig;
