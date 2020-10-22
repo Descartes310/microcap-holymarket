686b3f8983e94ec84d04ff0062b604b013fc827b
PatrissolJuns <patrissolkenfack@gmail.com>
2020-10-22 12:25:29 +0100

    Set up axios interceptors

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,51 @@
 import axios from 'axios';
+import AppConfig from 'Constants/AppConfig';
+import {toCamelCase, toSnakeCase} from "Helpers/helpers";
+import {getAuthToken} from "Helpers/tokens";
 
-export default
+const customAxios =
    axios.create({
-      baseURL: 'https://reactify.theironnetwork.org/data/',
-      timeout: 2000
+      baseURL: AppConfig.api.baseUrl,
+      timeout: 5000
    });
+
+customAxios.interceptors.request.use(
+    async (config) => {
+       try {
+          // Get auth token
+          const { accessToken } = getAuthToken();
+
+          if (accessToken && !config.shouldSkipToken) {
+             config.headers['Authorization'] = 'Bearer ' + accessToken;
+          }
+
+          if (config.data && !config.shouldSkip) {
+             config.data = toSnakeCase(config.data);
+          }
+
+          // config.headers['Content-Type'] = 'application/json';
+          return config;
+       } catch (e) {
+          return config;
+       }
+
+    },
+    error => Promise.reject(error));
+
+customAxios.interceptors.response.use(
+    response => {
+       if (response && response.data) {
+          if (Array.isArray(response.data)) {
+             response.data = response.data.map(item => toCamelCase(item));
+          } else if (response.data.hasOwnProperty('data')) {
+             if (Array.isArray(response.data.data)) {
+                // @ts-ignore
+                response.data.data = response.data.data.map(item => toCamelCase(item));
+             } else response.data.data = toCamelCase(response.data.data);
+          } else response.data = toCamelCase(response.data);
+       }
+
+       return response;
+    });
+
+export default customAxios;
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -78,3 +78,89 @@ export function getDefaultLanguage() {
 
     return defaultLanguage;
 }
+
+
+
+/**
+ * Deep mapping an object
+ * @param obj
+ * @param callback function to apply to the key
+ */
+export function deepMapObject(obj, callback) {
+    let rtn = obj;
+    if (typeof (obj) === 'object') {
+        if (obj instanceof Array) {
+            rtn = obj.map(deepMapObject);
+        } else {
+            rtn = {};
+            for (let key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    // apply the change on the string
+                    const newKey = callback(key);
+
+                    // Falsy or primitive value
+                    if (!obj[key] || typeof obj[key] !== 'object')
+                        rtn[newKey] = obj[key];
+                    // nested object
+                    else rtn[newKey] = deepMapObject(obj[key], callback);
+                }
+            }
+        }
+    }
+    return rtn;
+}
+
+/**
+ * Convert an object to camelCase
+ * @param obj
+ */
+export function toCamelCase(obj) {
+    // function to execute on each key
+    const callback = key => key.replace(/(_\w)/g, k => k[1].toUpperCase());
+
+    // call a generic method
+    return deepMapObject(obj, callback);
+}
+
+/**
+ * Convert an object to snake case
+ * @param obj
+ */
+export function toSnakeCase(obj) {
+    // function to execute on each key
+    const callback = key => key.replace(/\W+/g, " ")
+        .split(/ |\B(?=[A-Z])/)
+        .map(word => word.toLowerCase())
+        .join('_');
+
+    // call a generic method
+    return deepMapObject(obj, callback);
+}
+
+export const objectToFormData = (obj) => {
+    let formData = new FormData();
+    if (obj instanceof FormData) {
+        formData = obj;
+    } else {
+        Object.keys(obj).forEach(key => formData.append(key, obj[key]));
+    }
+    return formData;
+};
+
+/**
+ * Check if the user's value into store is valid
+ *
+ * @param authUser
+ * @param tokens
+ */
+export const isUserIntoStoreValid = (
+    authUser,
+    tokens,
+) => {
+    return ((authUser !== null &&
+            authUser !== undefined)
+        // ||
+        // (tokens.token !== null &&
+        //     tokens.token !== undefined)
+    );
+};
