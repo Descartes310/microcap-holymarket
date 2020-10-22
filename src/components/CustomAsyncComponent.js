24030645179aa32ff1a4f835ea753021dcbec446
PatrissolJuns <patrissolkenfack@gmail.com>
2020-10-22 12:52:45 +0100

    Created an AsyncComponent to handle Api based component

---
+++ src/components/CustomAsyncComponent.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
+import MatButton from "@material-ui/core/Button/Button";
+import {Alert} from "reactstrap";
+import LinearProgress from "@material-ui/core/LinearProgress/LinearProgress";
+
+const CustomAsyncComponent = ({loading, data, component, onRetryClick, errorMessageComponent}) => {
+
+    const _onRetryClick = (event) => {
+        event.preventDefault();
+        onRetryClick();
+    };
+
+    return (
+        <>
+            {loading ? (
+                <>
+                    <CircularProgress className="progress-primary mr-30 mb-10" />
+                    {/*<LinearProgress variant="query" />
+                    <br />
+                    <LinearProgress color="secondary" variant="query" />*/}
+                </>
+            ) : data ? (
+                <>
+                    {component(data)}
+                </>
+            ) : (
+                <Alert color="danger">
+                    {errorMessageComponent ? (
+                        <>
+                            {errorMessageComponent(_onRetryClick)}
+                        </>
+                    ) : (
+                       <>
+                           An error occur while fetching data <a href="#" className="alert-link text-decoration-underline" onClick={_onRetryClick}>Please try again</a>
+                       </>
+                    )}
+                </Alert>
+            )}
+        </>
+    );
+};
+
+CustomAsyncComponent.propTypes = {
+
+};
+
+export default CustomAsyncComponent;
