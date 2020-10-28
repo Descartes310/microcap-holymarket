5629690efa9cf364ea3dc8b4ecd99fcd6268fb0c
PatrissolJuns <patrissolkenfack@gmail.com>
2020-10-28 17:41:04 +0100

    Created Enums

---
+++ src/enums/Status.js
@@ -0,0 +1,7 @@
+import BaseEnum from './BaseEnum';
+
+export default class Status extends BaseEnum {
+    static PENDING = 'PENDING';
+    static EMPTY = 'EMPTY';
+    static CONFIRMED = 'CONFIRMED';
+}
