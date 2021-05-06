a05d9f731b2102b50aade57e4516fa7bb0a23189
Tessa Grace <gracetessa98@gmail.com>
2021-05-06 12:18:16 +0100

    design mobile and desktop nav

--- src/routes/custom/dashboard/DiscoverMenu.js
+++ src/routes/custom/dashboard/DiscoverMenu.js
@@ -6,7 +6,8 @@ import AppConfig from "Constants/AppConfig";
 import IntlMessages from "Util/IntlMessages";
 import Button from "@material-ui/core/Button";
 import {HashLink} from "react-router-hash-link";
-import {AGENTS, AUTH, GALERY_PROJECT, HOME, PASS_DETAILS} from "Url/frontendUrl";
+import {AGENTS, AUTH, GALERY_PROJECT, HOME, PASS_DETAILS, DISCOVER} from "Url/frontendUrl";
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Dropdown} from 'reactstrap';
 
 const MAX_MOBILE_SCREEN_WIDTH = 996;
 
@@ -27,7 +28,8 @@ class DiscoverMenu extends Component {
         super(props);
         this.state = {
             width: getWidth(),
-            showMobile: false
+            showMobile: false,
+            showMobileDorpdown:false
         }
     }
 
@@ -48,9 +50,14 @@ class DiscoverMenu extends Component {
         this.setState(state => ({showMobile: !state.showMobile}))
     };
 
+    onTClickDropdown = (event) => {
+        event.preventDefault();
+        this.setState(state => ({showMobileDorpdown: !state.showMobileDorpdown}))
+    };
+
 
     render() {
-        const { width, showMobile} = this.state;
+        const { width, showMobile, showMobileDorpdown} = this.state;
         const isMainNav = width > MAX_MOBILE_SCREEN_WIDTH;
 
         return (
@@ -66,34 +73,80 @@ class DiscoverMenu extends Component {
                         </div>
                         <div id="navbarContent">
                             <ul className="navbar-nav ml-auto">
-                                <li className="nav-item nav-item-border" style={{marginRight:"30px"}}>
-                                    <HashLink to={`${PASS_DETAILS}`}>
-                                        <a className="nav-link" href="#">
-                                            Decouvrir
-                                        </a>
-                                    </HashLink>
+                                <li className="nav-item">
+                                <Dropdown isOpen={showMobileDorpdown} toggle={(event)=> this.onTClickDropdown(event)} className="nav-item-border">
+                                    <DropdownToggle style={{background: "none", color: "lightslategray", border:"none", boxShadow: "none", color: "#464D69", padding: "0.70rem 1rem", fontSize: "inherit", fontWeight: "bold"}}>
+                                        Découvrir
+                                        <img class={showMobileDorpdown ? "inline-nav-arrow-final" : "inline-nav-arrow-initial"} src="https://sqy7rm.media.zestyio.com/Downward-Carat.svg" alt="Downward arrow"></img>
+                                    </DropdownToggle>
+                                    <DropdownMenu className="desktop-dropdown">
+                                        <DropdownItem>
+                                            <HashLink to={`${DISCOVER}/#produits`}>
+                                                <a className="nav-link-mobile-sub">
+                                                    Produits pour se financer
+                                                </a>
+                                            </HashLink>
+                                        </DropdownItem>
+                                        <DropdownItem>
+                                            <HashLink to={`${DISCOVER}/#investir`}>
+                                                <a className="nav-link-mobile-sub ">
+                                                    Produits pour investir
+                                                </a>
+                                            </HashLink>
+                                        </DropdownItem>
+                                        <DropdownItem>
+                                            <HashLink to={`${DISCOVER}/#services`}>
+                                                <a className="nav-link-mobile-sub ">
+                                                    Produits des Partenaires
+                                                </a>
+                                            </HashLink>
+                                        </DropdownItem>
+                                        
+                                        <DropdownItem>
+                                            <HashLink to={`${DISCOVER}/#pass`}>
+                                                <a className="nav-link-mobile-sub">
+                                                    Pass Microcap
+                                                </a>
+                                            </HashLink>
+                                        </DropdownItem>
+                                        <DropdownItem>
+                                            <HashLink to={`${DISCOVER}/#pioniers`}>
+                                                <a className="nav-link-mobile-sub">
+                                                    L'équipe
+                                                </a>
+                                            </HashLink>
+                                        </DropdownItem>
+                                        <DropdownItem>
+                                            <HashLink to={`${DISCOVER}/#agents`}>
+                                                <a className="nav-link-mobile-sub">
+                                                    Point d'accueil
+                                                </a>
+                                            </HashLink>
+                                        </DropdownItem>
+                                    </DropdownMenu>
+                                </Dropdown>
                                 </li>
-                                <li className="nav-item nav-item-border" style={{marginRight:"30px"}}>
+                                <li className="nav-item nav-item-border" >
                                     <HashLink to={`${PASS_DETAILS}`}>
-                                        <a className="nav-link" href="#">
+                                        <a className="nav-link-mobile" href="#">
                                             Pass microcap
                                         </a>
                                     </HashLink>
                                 </li>
-                                <li className="nav-item nav-item-border" style={{marginRight:"30px"}}>
+                                <li className="nav-item nav-item-border">
                                     <Link to={GALERY_PROJECT}>
-                                        <a className="nav-link" href="#">Gallerie projets</a>
+                                        <a className="nav-link-mobile" href="#">Gallerie projets</a>
                                     </Link>
                                 </li>
-                                <li className="nav-item nav-item-border" style={{marginRight:"30px"}}>
+                                <li className="nav-item nav-item-border">
                                     <HashLink to={`${AGENTS}`}>
-                                        <a className="nav-link" href="#">Réseau d'agent</a>
+                                        <a className="nav-link-mobile" href="#">Réseau d'agent</a>
                                     </HashLink>
                                 </li>
                                 <li>
                                     <Button
                                         variant="contained"
-                                        className="btn-primary mr-2"
+                                        className="btn-primary ml-30 mr-2"
                                         onClick={() => props.history.push(AUTH.LOGIN)}>
                                         <IntlMessages id="auth.signin" />
                                     </Button>
@@ -103,6 +156,7 @@ class DiscoverMenu extends Component {
                         </div>
                     </div>
                 </nav>
+                
 
                 <nav
                     className={`bg-light ${!isMainNav ? 'show-mobile-nav' : ''}`}
@@ -125,28 +179,62 @@ class DiscoverMenu extends Component {
                         {showMobile ? (
                             <div id="mobile-center">
                                 <ul className="">
-                                    <li className="nav-item nav-item-border" style={{marginRight:"30px"}}>
+                                    <li className="nav-item" style={{marginRight:"30px"}}>
                                         <HashLink to={`${PASS_DETAILS}`}>
-                                            <a className="nav-link" href="#">
+                                            <a className="nav-link-mobile" href="#" 
+                                            onClick={(event)=>this.onTClickDropdown(event)}>
                                                 Découvrir
+                                                <img class={showMobileDorpdown ? "inline-nav-arrow-final" : "inline-nav-arrow-initial"} src="https://sqy7rm.media.zestyio.com/Downward-Carat.svg" alt="Downward arrow"></img>
                                             </a>
+                                            <SlideDown className={'second-dropdown-mobile'}>
+                                                {showMobileDorpdown ? ( 
+                                                <div id="dropdown-mobile-center">
+                                                    <ul>
+                                                        <li className="nav-item nav-item-border">
+                                                            <a className="nav-link-mobile ml-30" href="#">
+                                                                Produits pour se financer
+                                                            </a>
+                                                        </li>
+                                                        <li className="nav-item nav-item-border">
+                                                            <a className="nav-link-mobile ml-30" href="#">
+                                                                Produits pour investir
+                                                            </a>
+                                                        </li>
+                                                        <li className="nav-item nav-item-border">
+                                                            <a className="nav-link-mobile ml-30" href="#">
+                                                                Produits des Partenaires
+                                                            </a>
+                                                        </li>
+                                                        <li className="nav-item nav-item-border">
+                                                            <a className="nav-link-mobile ml-30" href="#">
+                                                                Pass Microcap
+                                                            </a>
+                                                        </li>
+                                                        <li className="nav-item nav-item-border">
+                                                            <a className="nav-link-mobile ml-30" href="#">
+                                                                L'équipe
+                                                            </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>) : null}
+                                            </SlideDown>
                                         </HashLink>
                                     </li>
                                     <li className="nav-item nav-item-border" style={{marginRight:"30px"}}>
                                         <HashLink to={`${PASS_DETAILS}`}>
-                                            <a className="nav-link" href="#">
+                                            <a className="nav-link-mobile" href="#">
                                                 Pass microcap
                                             </a>
                                         </HashLink>
                                     </li>
                                     <li className="nav-item nav-item-border" style={{marginRight:"30px"}}>
                                         <Link to={GALERY_PROJECT}>
-                                            <a className="nav-link" href="#">Gallerie projets</a>
+                                            <a className="nav-link-mobile" href="#">Gallerie projets</a>
                                         </Link>
                                     </li>
                                     <li className="nav-item nav-item-border" style={{marginRight:"30px"}}>
                                         <HashLink to={`${AGENTS}`}>
-                                            <a className="nav-link" href="#">Réseau d'agent</a>
+                                            <a className="nav-link-mobile" href="#">Réseau d'agent</a>
                                         </HashLink>
                                     </li>
                                     <li>
