143bd65ce631b7f6bdd21f26156c7c7a77d75ab0
Tessa Grace <gracetessa98@gmail.com>
2021-05-07 11:52:54 +0100

    design video text content

--- src/routes/custom/dashboard/DiscoverVideo.js
+++ src/routes/custom/dashboard/DiscoverVideo.js
@@ -46,27 +46,27 @@ class DiscoverVideo extends Component {
         
 
         return (
-
-        <div className="container">
-            <div className="row" >
-                <div className="col-md-6">
-                    <p style={{ fontSize: '1.2em', marginTop: 20, textAlign: 'center' }}>
-                        Microcap vous accompagne dans la réalisation de votre projet:  création ou développement d’entreprise, actionnariat, formation à l’entrepreneuriat ...<br />
-                        <Link to={AUTH.REGISTER} style={{ color: '#e19d00' }}>İnscrivez-vous</Link> et choississez l’abonnement qui vous correspond parmi <Link to={PASS_DETAILS} style={{ color: '#e19d00' }}>nos PASS</Link>
+            <div className="row video" >
+                <div className="col-md-6 video-text-content">
+                    <p className="text-center">
+                        Microcap vous accompagne dans la réalisation
+                        de votre projet:  création ou développement
+                        d’entreprise, actionnariat, formation à l’entrepreneuriat ...
+                        <br />
+                        <Link to={AUTH.REGISTER} style={{ color: '#e19d00' }}>
+                            İnscrivez-vous</Link> et choississez l’abonnement
+                        qui vous correspond parmi
+                        <Link to={PASS_DETAILS} style={{ color: '#e19d00' }}>
+                            nos PASS</Link>
                     </p>
                 </div>
                 <div className="col-md-6">
-                    <img src={require('Assets/img/microcap.png')} style={{width:"100%"}}/>
+                    <img src={require('Assets/img/microcap.png')} className="img-fluid"/>
                     <div className="text-center btn-over">
                         <a className="btn-round" onClick={(event)=>this.onModal(event)} >
-                            <i className="ti-control-play font-2x" aria-hidden="true"></i></a>
+                            <i className="ti-control-play font-2x" aria-hidden="true"/>
+                        </a>
                     </div>
-                    {/* <Player
-                        playsInline
-                        poster={require('Assets/img/microcap.png')}
-                        //src='http://api-preprod.microcap.fr/files/videos/video.mp4'
-                        src='http://api.microcap.fr/files/videos/video.mp4'
-                    /> */}
                 </div>
             
             <Dialog
@@ -75,28 +75,28 @@ class DiscoverVideo extends Component {
                 aria-labelledby="alert-dialog-title"
                 fullWidth='false'
                 maxWidth='md'  
-                
+                className="main-dialog"
             >
                 <DialogTitle id="alert-dialog-title">
-                    <a class="" onClick={()=>this.handleCloseConfirmationAlert()} >
-                            <i className="ti-close font-2x" aria-hidden="true"></i>
+                    <div className="close-cross">
+                        <a className="" onClick={()=>this.handleCloseConfirmationAlert()} >
+                            <i className="ti-close font-2x" aria-hidden="true"/>
                         </a>
+                    </div>
                 </DialogTitle>
                 <DialogContent>
                     
                         <Player
-                            playsInline
+                            autoPlay
                             poster={require('Assets/img/microcap.png')}
                             //src='http://api-preprod.microcap.fr/files/videos/video.mp4'
                             src='http://api.microcap.fr/files/videos/video.mp4'
-                            
                         /> 
                       
                 </DialogContent>
             
             </Dialog>
             </div>
-        </div>
         );
     }
 }
