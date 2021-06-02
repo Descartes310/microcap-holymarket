1fa37f3257ba3bc9882cfc74f2baa183b40aa494
PatrissolJuns <patrissolkenfack@gmail.com>
2021-06-02 16:32:53 +0100

    Applied changes of community space url to admin/projects components

--- src/routes/custom/communityT/projects/index.js
+++ src/routes/custom/communityT/projects/index.js
@@ -3,31 +3,19 @@ import Update from './Update';
 import Gallery from './Gallery';
 import React, { Component } from 'react';
 import { COMMUNITY } from "Url/frontendUrl";
-import { Scrollbars } from 'react-custom-scrollbars';
 import { withRouter, Switch, Redirect, Route } from "react-router-dom";
 
-
 const FoldersManagement = ({ match }) => {
-
     return (
         <div className="list-wrap">
-            <Scrollbars
-                className="rct-scroll"
-                autoHide
-                style={{ height: 'calc(100vh - 128px)', position: 'unset', padding: 24 }}
-            >
-                <React.Fragment>
-                    <Switch>
-                        <Redirect exact from={`${match.url}/`} to={COMMUNITY.PROJECTS.SHOW} />
-                        <Route path={COMMUNITY.PROJECTS.GALLERY} component={Gallery} />
-                        <Route path={COMMUNITY.PROJECTS.SHOW} component={Show} />
-                        <Route path={COMMUNITY.PROJECTS.UPDATE} component={Update} />
-                    </Switch>
-                </React.Fragment>
-            </Scrollbars>
+            <Switch>
+                <Redirect exact from={`${match.url}/`} to={COMMUNITY.PROJECTS.SHOW} />
+                <Route path={COMMUNITY.PROJECTS.GALLERY} component={Gallery} />
+                <Route path={COMMUNITY.PROJECTS.SHOW} component={Show} />
+                <Route path={COMMUNITY.PROJECTS.UPDATE} component={Update} />
+            </Switch>
         </div>
     );
-}
-
+};
 
 export default withRouter(FoldersManagement);
