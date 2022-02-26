380aa6197728d7667bbe3d04a854188263b04da3
Descartes Fowo <descartesouambo@gmail.com>
2022-02-26 16:11:33 +0100

    Display calendar

--- src/routes/custom/dashboard/landing/Events.js
+++ src/routes/custom/dashboard/landing/Events.js
@@ -1,5 +1,6 @@
-import React from 'react';
 import moment from 'moment';
+import SettingService from 'Services/settings';
+import React, {useState, useEffect} from 'react';
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
 import DiscoverLayout from "Routes/custom/dashboard/landing/discover/DiscoverLayout";
 
@@ -9,7 +10,7 @@ const allViews = Object.keys(Views).map(k => Views[k]);
 const ColoredDateCellWrapper = ({ children }) =>
   React.cloneElement(React.Children.only(children), {
     style: {
-      backgroundColor: 'lightblue',
+      backgroundColor: '#FFB70F',
     },
 });
 
@@ -17,18 +18,43 @@ const Localizer = momentLocalizer(moment);
 
 const Events = () => {
 
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        getEvents();
+        console.log(moment('2022-03-12').toDate());
+    }, []);
+
+    const getEvents = () => {
+        SettingService.getEvents()
+        .then((response) => setEvents(response))
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
     document.body.style.overflow = "auto";
 
     return (
       <DiscoverLayout style={{ paddingTop: '24vh', backgroundColor: 'white' }}>
-        <div className='container'>
+        <div className='container mb-70'>
+          <div className="showcase-card-block d-flex flex-column pb-0" style={{ padding: '0vh 10vw' }}>
+            <div className='container'>
+              <div className="row center-hor-ver mb-70 flex-column intro">
+                <h2 className="font-weight-bold text-black text-center underline-title mb-50" data-aos="fade-right">
+                    Evènements MicroCap
+                </h2>
+              </div>
+            </div>
+          </div>
           <Calendar
             localizer={Localizer}
-            events={[]}
+            events={events.map(e => { 
+              return { title: e.label, start: moment(e.startDate).toDate(), end: e.endDate ? moment(e.endDate).toDate() : moment(e.startDate).toDate(), desc: e.description}
+            })}
             views={allViews}
             step={60}
-            showMultiDayTimes
-            defaultDate={new Date(2015, 3, 1)}
+            defaultDate={new Date()}
             components={{
             timeSlotWrapper: ColoredDateCellWrapper,
             }}
