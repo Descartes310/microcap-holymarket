3fa99072bb0ffdba28e51fae2103ea7cce1a4887
Descartes Fowo <descartesouambo@gmail.com>
2023-10-11 16:17:45 +0100

    Fix cart issue

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -25,12 +25,12 @@ export default (state = INIT_STATE, action) => {
 
 	switch (action.type) {
 		case CART_ADD_ITEM:
-			console.log("obj", obj);
-			console.log("obj.data", obj.data);
-			console.log("action", action);
-			console.log("action.authId", action.authId);
-			console.log("state.items", state.items);
-			console.log("action.payload", action.payload);
+			console.log(obj);
+			console.log(obj.data);
+			console.log(action);
+			console.log(action.authId);
+			console.log(state.items);
+			console.log(action.payload);
 			obj.data[action.authId] = [...state.items, action.payload];
 			return new Cart(obj);
 
